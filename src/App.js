8e7cab33df14cc538fbe0a40d4ebba390e048f9e
ItsMario25 <mmariohernandez58@gmail.com>
2024-04-10 10:52:50 -0500

    añado dinamismo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/welcome';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import Contact from './pages/Contact';
+import Aboutme from './pages/Aboutme';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -18,8 +21,19 @@ function App() {
               <li>
                 <Link to="/contact">Contactos</Link>
               </li>
+              <li>
+              <Link to="/aboutme">About me</Link>
+              </li>
+              <li>
+              <Link to="/profile">Profile</Link>
+              </li>
             </ul>
           </div>
+          <Routes>
+            <Route path='/contact' element={<Contact />}></Route>
+            <Route path='/aboutme' element={<Aboutme />}></Route>
+            <Route path='/profile/:name' element={<Profile />}></Route>
+          </Routes>
         </Router>
       </header>
     </div>
---
+++ src/pages/Profile.js
@@ -0,0 +1,10 @@
+import { useParams} from 'react-router-dom'
+export default function Profile(){
+    const { name } = useParams();
+    console.log(name);
+    return(
+        <div>
+            <h4>Desde profile, con el nombre de {name}</h4>
+        </div>
+    )
+};
