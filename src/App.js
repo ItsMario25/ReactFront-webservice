2563672a4c524420a34f046bddeb8351ff36a6d3
ItsMario25 <mmariohernandez58@gmail.com>
2024-04-12 11:09:01 -0500

    nuevas incorporaciones

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import Welcome from './components/welcome';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import Contact from './pages/Contact';
 import Aboutme from './pages/Aboutme';
 import Profile from './pages/Profile';
+import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Welcome message="Hola perros" />
         <Router>
           <div>
             <ul>
@@ -35,9 +32,35 @@ function App() {
             <Route path='/profile/:name' element={<Profile />}></Route>
           </Routes>
         </Router>
+        <Container>
+        <Row className="justify-content-center mt-5">
+          <Col xs={10} sm={8} md={6}>
+            <h2 className="text-center">Login</h2>
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit" block>
+                Submit
+              </Button>
+            </Form>
+            <div className="mt-3 text-center">
+              <a href="#">Forgot Password?</a>
+            </div>
+          </Col>
+        </Row>
+       </Container>
       </header>
     </div>
   );
 }
+
 
 export default App;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import { useParams} from 'react-router-dom'
+
 export default function Profile(){
     const { name } = useParams();
     console.log(name);
